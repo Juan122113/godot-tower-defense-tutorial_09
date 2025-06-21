805e6cee703b067f444aeef835c74c25efb8c709
Juan122113 <ammiel89@hotmail.com>
2025-06-21 00:14:45 -0300

    Cleaning the code

--- scripts/base.gd
+++ scripts/base.gd
@@ -9,9 +9,7 @@ extends Node3D
 #@onready var area_3d: EnemyArea3d = $Path3D/PathFollow3D/Area3D
 
 var base_health:float
-#func _on_area_3d_area_entered(area: Area3D) -> void:
-	#if area is Enemy:
-		#base_settings.health -= area.damage
+
 		
 func _ready():
 #	print("Ready")
@@ -23,29 +21,9 @@ func _ready():
 func _process(delta: float) -> void:
 	_on_base_destruction()
 
-#func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
-	#if body is Enemy:
-		#print("Colisionó")
-		#base_health -= body.damage
-		#progress_bar.value = base_health
-#
-#
-#func _on_area_3d_body_entered(body: Node3D) -> void:
-	#if body is Node3D:
-		#print("Colisionó")
-		#base_health -= body.damage
-		#progress_bar.value = base_health
-
-
-#func _on_area_3d_area_entered(area: Area3D) -> void:
-	#if area is Area3D:
-	#print(area, "Colisionó")
-	#base_health -= area.enemy_01.damage
-	#progress_bar.value = base_health
-
 
 func _on_base_area_area_entered(area: Area3D) -> void:
-	print(area, "Colisionó")
+	#print(area, "Colisionó")
 	base_health -= area.enemy_settings.damage
 	progress_bar.value = base_health
 	
